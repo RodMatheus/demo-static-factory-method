73c844a5f2c21be21010fae11aca7f699c86f5ed
Matheus Rodrigues <rodmatheus96@gmail.com>
2022-03-11 17:42:38 -0300

    Correção de nome do projeto

--- src/main/java/com/example/demo/DemoStaticFactoryMethodApplication.java
+++ src/main/java/com/example/demo/DemoStaticFactoryMethodApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoBuilderPatternApplication {
+public class DemoStaticFactoryMethodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoBuilderPatternApplication.class, args);
+		SpringApplication.run(DemoStaticFactoryMethodApplication.class, args);
 	}
 
 }
